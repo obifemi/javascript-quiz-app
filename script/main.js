907275f12a93559b38af3f857b202187a25bfc29
ali mert inal <mert.inal@gmail.com>
2023-06-06 22:56:25 +0200

    add live

--- script/main.js
+++ script/main.js
@@ -1,32 +1,4 @@
-const questions = [
-    {
-        id: '1',
-        name: 'name',
-        message: 'What is your name?', 
-    },
-    {
-        type: '2',
-        name: 'location',
-        message: 'Where are you from?',
-    },
-    {
-        type: '3',
-        name: 'hobby',
-        message: 'What is your favorite hobby?',
-    },
-    {
-        type: '4',
-        name: 'food',
-        message: 'What is your favorite food?',
-    },
-    {
-        type: '5',
-        name: 'github',
-        message: 'Enter your GitHub Username',
-    }
-];
 
-const answers = [];
 
 user = {
     name: '',
